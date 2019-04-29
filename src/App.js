bdb0539bc71b098a3b89834f8c69f18012eab251
gregatgit <gregdd@outlook.com>
2019-04-29 15:10:39 +1000

    add icons to home page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ function App() {
   return (
     <div>
       <h2>my App!</h2>
+      <div>
+        <i aria-hidden="true" className="red huge cart icon" />
+        <div>
+          
+          <i aria-hidden="true" className="home huge icon" />
+          <br />
+
+        </div>
+      </div>
     </div>
   )
 }
