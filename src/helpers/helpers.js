0da1525b757e203cec2f2107e17f64ce5c5efcdd
gregatgit <gregdd@outlook.com>
2019-05-02 10:50:11 +1000

    spelling

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -4,7 +4,7 @@ export const displayName = (name) => {
   return name.replace(/_/g, " ").toUpperCase()
 }
 
-// returns an array of items that have matching code
+// returns an array of items that have matching list-code
 export const getItems = (list, code) => {
   return list.filter(item => {
     return item.lists.indexOf(code) > -1
