ffc5607c44831dd7bc06f380e30ae87f9d94cd33
gregatgit <gregdd@outlook.com>
2019-05-08 10:59:11 +1000

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-// import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 import promise from 'redux-promise'
 import reducers from './reducers'
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore)
 
-ReactDOM.render(<Provider store={createStoreWithMiddleware(reducers, window.__REDUX_DEVTOOLS_EXTENSION__&& window.__REDUX_DEVTOOLS_EXTENSION__())}>
-  <App />
-</Provider>, document.getElementById('root'));
-
+ReactDOM.render(
+  <Provider
+    store={createStoreWithMiddleware(
+      reducers,
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}
+  >
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
