21aedc158a0f4bfcfc1ab94a757b96e0874d6d86
gregatgit <gregdd@outlook.com>
2019-09-19 11:00:58 +1000

    reset App

--- src/App.js
+++ src/App.js
@@ -1,35 +1,15 @@
-import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import React, { Component } from 'react'
 import HomePage from './components/HomePage'
-import MyList from './components/MyList'
-import FinalShop from './components/FinalShop'
-import NewItem from './components/NewItem'
-import NewItemForm from './components/NewItemForm'
 
-function App() {
-  return (
-    <div>
+class App extends Component {
+  state = {display: 'HomePage', user: 'Greg'}
+  render () {
+    return (
       <div>
-        <h2>GO SHOP</h2>
-        <div>
-          <i aria-hidden="true" className="red huge cart icon" />
-          <div />
-        </div>
+        <HomePage />
       </div>
-      <div>
-        <BrowserRouter>
-          <div>
-            <Route path="/" exact component={HomePage} />
-            <Route path="/page2" component={MyList} />
-            <Route path="/shopping" component={FinalShop} />
-            <Route path="/newItem" component={NewItem} />
-            <Route path="/new" component={NewItemForm} />
-          </div>
-        </BrowserRouter>
-      </div>
-      <br />
-    </div>
-  )
+    )
+  }
 }
 
 export default App
