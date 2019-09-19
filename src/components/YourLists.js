15462a8f9975e75f07b10e369d1665ffaa42e51e
gregatgit <gregdd@outlook.com>
2019-09-19 14:44:45 +1000

    choose a list to load

--- src/components/YourLists.js
+++ src/components/YourLists.js
@@ -1,28 +1,41 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import { connect } from 'react-redux'
+import { displayName } from '../helpers'
 
 const YourLists = ({ lists }) => {
+  const [choosen, setChoosen] = useState(null)
 
-  const createListButton = (list) => {
+  function handleListClick(name){
+    setChoosen(name)
+  }
+
+  function loadList(list){
+    console.log(list)
+  }
+
+  const createListButton = list => {
     const listOfList = Object.keys(list)
     return listOfList.map(name => {
+      const display = displayName(lists[name].name)
       return (
         <li key={name}>
-          <button className="myButton">{lists[name].name}</button>
+          <button onClick={() => handleListClick(name)} className="myButton">{display}</button>
         </li>
       )
     })
   }
   return (
-    <ul>
-      {lists && createListButton(lists)}
-    </ul>
+    <Fragment>
+      <h2>Choose A List</h2>
+      <ul>{lists && createListButton(lists)}</ul>
+      {choosen && <button onClick={() => loadList(choosen)}>LOAD {displayName(lists[choosen].name)}</button>}
+    </Fragment>
   )
 }
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    lists: state.lists
+    lists: state.lists,
   }
 }
 
