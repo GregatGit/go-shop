b855fd83741d7fb2ce3b3e54a43fb9d3cf7567d1
gregatgit <gregdd@outlook.com>
2019-09-19 19:30:06 +1000

    dislay item in main shopping list

--- src/components/TheLists/ShoppingList.js
+++ src/components/TheLists/ShoppingList.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const ShoppingList = () => {
+const ShoppingList = ({ list }) => {
+  const displayList = sList => {
+    return sList.map(item => {
+      return (
+        <li className="sList" key={item.name}>
+          {item.name}
+        </li>
+      )
+    })
+  }
   return (
     <div>
       <h2>SHOPPING LIST</h2>
+      {list && displayList(list)}
     </div>
   )
 }
 
-export default ShoppingList
+const mapStateToProps = (state, ownProps) => {
+  return {
+    list: state.shoppingList
+  }
+}
+
+export default connect(mapStateToProps)(ShoppingList)
