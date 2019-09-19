f284b1c52eced4ff3bfee4eb3a720bc35b83c61d
gregatgit <gregdd@outlook.com>
2019-09-19 11:15:16 +1000

    reset homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,64 +1,29 @@
 import React from 'react'
-import _ from 'lodash'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
 import { selectChosen, filterList } from '../actions'
-import { displayName } from '../helpers'
 
 class HomePage extends React.Component {
-  state = { message: 'Pick a list', list: 'none', code: '' }
+  state = { user: 'Greg' }
 
-  listSegments = _.map(this.props.lists, list => {
-    return (
-      <div
-        onClick={() => {
-          this.setState({
-            message: list.description,
-            list: displayName(list.name),
-            code: list.code,
-          })
-          this.props.selectChosen(list.code)
-        }}
-        key={list.code}
-        className="ui segment"
-      >
-        {displayName(list.name)}
-        <button className="ui blue right floated button">O</button>
-      </div>
-    )
-  })
   render() {
     return (
-      <div className="ui equal width grid">
-        <div className="stretched row">
-          <div className="column">{this.listSegments}</div>
-          <div className="six wide column">
-            <div className="ui segment">
-              <div>{this.state.message}</div>
-              <br />
-              {this.state.list === 'none' ? (
-                ''
-              ) : (
-                <Link to="/page2">
-                  <button
-                    onClick={() => this.props.filterList(this.state.code)}
-                    className="ui fluid button"
-                  >
-                    LOAD <u>{this.state.list}</u> LIST
-                  </button>
-                </Link>
-              )}
-            </div>
-          </div>
-          <div className="column">
-            <div className="ui segment">
-              <h3>CREATE A BRAND NEW LIST</h3>
-              <br />
-              <button className="ui fluid button">NEW LIST</button>
-            </div>
-            <div className="ui segment">2</div>
-          </div>
-        </div>
+      <div>
+        <h2>Hi {this.state.user}</h2>
+        <ul>
+          <li>
+            <button>QUICK LISTS</button>
+          </li>
+          <li>
+            <button>YOUR LISTS</button>
+          </li>
+          <li>
+            <button>MAKE A LIST</button>
+          </li>
+          <li>
+            <button>IMPORT A LIST</button>
+          </li>
+          <li><button>SEND A LIST</button></li>
+        </ul>
       </div>
     )
   }
