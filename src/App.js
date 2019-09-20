8f329618e891c2eb4a2d5b8f763fc049a403b201
gregatgit <gregdd@outlook.com>
2019-09-20 11:25:18 +1000

    removed class and unused state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react'
 import HomePage from './components/HomePage'
 
-class App extends Component {
-  state = {display: 'HomePage', user: 'Greg'}
-  render () {
-    return (
-      <div>
-        <HomePage />
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  )
 }
 
 export default App
