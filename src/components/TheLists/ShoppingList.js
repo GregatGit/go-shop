f4d32ba048ae6f916ce420cebf58d326ad90bb55
gregatgit <gregdd@outlook.com>
2019-09-22 23:01:39 +1000

    display items bought

--- src/components/TheLists/ShoppingList.js
+++ src/components/TheLists/ShoppingList.js
@@ -1,14 +1,28 @@
-import React, { useState} from 'react'
+import React, { useState } from 'react'
 import { connect } from 'react-redux'
-import { itemBought, homePage, completedList, emptyShoppingList } from '../../actions'
+import {
+  itemBought,
+  homePage,
+  completedList,
+  emptyShoppingList,
+} from '../../actions'
 import { displayName } from '../../helpers'
 
-const ShoppingList = ({ list, itemBought, homePage, completedList, emptyShoppingList }) => {
+const ShoppingList = ({
+  list,
+  itemBought,
+  homePage,
+  completedList,
+  emptyShoppingList,
+}) => {
   const [count, setCount] = useState(list.length)
+  const [bought, setBought] = useState(0)
+  const [showBought, setShowBought] = useState(false)
 
-  const itemClick = (name) => {
+  const itemClick = name => {
     itemBought(name)
     setCount(count - 1)
+    setBought(bought + 1)
   }
 
   const done = () => {
@@ -27,14 +41,33 @@ const ShoppingList = ({ list, itemBought, homePage, completedList, emptyShopping
         )
       })
   }
+  const displayBought = list => {
+    return list
+      .filter(item => item.done)
+      .map(item => {
+        return <li key={item.name}>{item.name}</li>
+      })
+  }
+  function handleBought() { 
+    setShowBought(!showBought)
+  }
   return (
     <div>
       <h2>SHOPPING LIST</h2>
-      <ul>
-      {list && displayList(list)}
-      </ul>
-      {count > 0 ? <p>items left: <b>{count}</b></p>
-        : <button onClick={done}>DONE</button>}
+      <ul>{list && displayList(list)}</ul>
+      {count > 0 ? (
+        <p>
+          items left: <b>{count}</b>
+        </p>
+      ) : (
+        <button onClick={done}>DONE</button>
+      )}
+      <div>
+        <button onClick={handleBought}>You bought {bought}</button>
+      </div>
+      <div>
+        {showBought ? displayBought(list) : 'click to see items bought'}
+      </div>
     </div>
   )
 }
