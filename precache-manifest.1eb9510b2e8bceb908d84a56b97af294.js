2f79617e5223522144342bb123a978b83edf6a15
gregatgit <gregdd@outlook.com>
2019-09-23 19:54:32 +1000

    Updates

---
+++ precache-manifest.1eb9510b2e8bceb908d84a56b97af294.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0742405c8c3452c729fac8da3dc04d45",
+    "url": "/go-shop/index.html"
+  },
+  {
+    "revision": "7ad4effbffc5d7ab7855",
+    "url": "/go-shop/static/css/main.d8718658.chunk.css"
+  },
+  {
+    "revision": "491b7fd4430889d4fd45",
+    "url": "/go-shop/static/js/2.d723aa4f.chunk.js"
+  },
+  {
+    "revision": "7ad4effbffc5d7ab7855",
+    "url": "/go-shop/static/js/main.65c663c6.chunk.js"
+  },
+  {
+    "revision": "c394c5fbe9408ec7516c",
+    "url": "/go-shop/static/js/runtime~main.882f2166.js"
+  }
+]);
