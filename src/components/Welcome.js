edcc5d39b50e413e4a93d3d16dd36572d74e90ac
gregatgit <gregdd@outlook.com>
2019-09-27 16:10:05 +1000

    a new welcome message with info

---
+++ src/components/Welcome.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Welcome = () => {
+  return (
+    <div>
+      <h1>GO SHOP</h1>
+      <p>Welcome to go shop.</p>
+      <p>Create your own list for easy shopping.</p>
+      <p>You can add and remove items to your list from other lists.</p>
+      <p>Once your happy with your list you're ready to shop.</p>
+      <p>To start off press the LISTS button below</p>
+    </div>
+  )
+}
+
+export default Welcome
