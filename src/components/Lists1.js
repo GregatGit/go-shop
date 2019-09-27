4e1034292293f4f396011ee50df53cba4541ff97
gregatgit <gregdd@outlook.com>
2019-09-27 16:10:24 +1000

    lists new home

---
+++ src/components/Lists1.js
@@ -0,0 +1,41 @@
+import React, { Fragment, useState } from 'react'
+import { connect } from 'react-redux'
+import { selectChosen } from '../actions'
+import { displayName } from '../helpers'
+
+const Lists = ({ lists, selectChosen, home, amount }) => {
+  const createListButton = list => {
+    const listOfList = Object.keys(list)
+    return listOfList.map(name => {
+      const display = displayName(lists[name].name)
+      return (
+        <li key={name}>
+          <button className="myButton">
+            {display}
+          </button>
+        </li>
+      )
+    })
+  }
+
+  return (
+    <div>
+      <h2>LISTS</h2>
+      <h3>press to see items in list</h3>
+      <p>they won't show if they are already on your list</p>
+      {createListButton(lists)}
+    </div>
+  )
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    lists: state.lists,
+    amount: state.shoppingList.length
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { selectChosen }
+)(Lists)
