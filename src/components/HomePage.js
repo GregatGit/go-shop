7787b237002f185552f48495f93d535e4734e64e
gregatgit <gregdd@outlook.com>
2019-09-27 16:08:49 +1000

    New home page with react router

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,38 +1,42 @@
-import React from 'react'
+import React, { Fragment, Component } from 'react'
 import { connect } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { selectChosen, filterList } from '../actions'
-import Lists from './Lists'
+import Lists from './Lists1'
+import Footer from './Footer'
+import Welcome from './Welcome'
 import YourLists from './YourLists'
 import TheList from './TheLists/Index'
 import './styles.scss'
 
-class HomePage extends React.Component {
+class HomePage extends Component {
   state = { user: 'Greg', show: 'Lists' }
 
-  changeShow = (name) => {
-    this.setState({show: name})
+  changeShow = name => {
+    this.setState({ show: name })
   }
   home = () => {
-    this.setState({show: 'Lists'})
+    this.setState({ show: 'Lists' })
   }
   render() {
-    const { chosen } = this.props
-    if (!chosen){
     return (
-      <div>
-        {this.state.show === 'Lists' && <Lists func={this.changeShow} name={this.state.user} />}
-        {this.state.show === 'YourLists' && <YourLists home={this.home} />}
-      </div>
-    )} else {
-      return <TheList />
-    }
+      <Fragment>
+        <BrowserRouter>
+          <Route path='/' exact component={Welcome} />
+          <Route path='/lists' component={Lists} />
+          <Route path='/' component={Footer} />
+        </BrowserRouter>
+        
+      </Fragment>
+    )
   }
 }
+
 
 const mapStateToProps = (state, ownProps) => {
   return {
     lists: state.lists,
-    chosen: state.chosen
+    chosen: state.chosen,
   }
 }
 
