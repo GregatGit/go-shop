cf0937449c295f0a6f5aa4a1451640c63b5daba4
gregatgit <gregdd@outlook.com>
2019-09-29 20:22:36 +1000

    create function to generate list

--- src/components/MyList.js
+++ src/components/MyList.js
@@ -2,15 +2,20 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 const MyList = ({list}) => {
+  function displayItems(list) {
+    return list.map(item => {
+      return (
+        <li key={item}>{item.name}</li>
+      )
+    })
+  }
   return (
     <div>
-      My List
+      <h2>YOUR LIST</h2>
+      <p>Use the buttons to remove items or mark them as done.</p>
+      <p>You must click on CHANGE LIST before you leave this page</p>
       <ul>
-        {list.map((item, index) => {
-          return (
-            <li key={index}>{item.name}</li>
-          )
-        })}
+        {list.length ? displayItems(list) : 'your list is empty'}
       </ul>
     </div>
   )
