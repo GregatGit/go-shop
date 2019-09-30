cfb52b98342c495a036b088d4c64d3bf97826eeb
gregatgit <gregdd@outlook.com>
2019-09-30 11:05:44 +1000

    add instructions

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,6 +7,7 @@ const Welcome = () => {
       <p>Welcome to go shop.</p>
       <p>Create your own list for easy shopping.</p>
       <p>You can add and remove items to your list from other lists.</p>
+      <p>At MY LIST you can see what is on your list at any time and remove items or mark them as bought or not bought.</p>
       <p>Once your happy with your list you're ready to shop.</p>
       <p>To start off press the LISTS button below</p>
     </div>
