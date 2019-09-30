c466bb4f979470263d64507f03eda89cc1864e92
gregatgit <gregdd@outlook.com>
2019-09-30 11:50:54 +1000

    add material ui icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,76 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { makeStyles } from '@material-ui/core/styles'
+import { AppBar, Tabs, Tab, Typography, Box } from '@material-ui/core'
 import { Link } from 'react-router-dom'
 
-const Header = () => {
+function TabPanel(props) {
+  const { children, value, index, ...other } = props
+
   return (
-    <header>
-      <Link to="/go-shop/">
-        <button>HOME</button>
-      </Link>
-      <Link to="/go-shop/lists">
-        <button>LISTS</button>
-      </Link>
-      <Link to="/go-shop/myList">
-        <button>MY LIST</button>
-      </Link>
-      <Link to="/go-shop/shoppingList">
-        <button>LETS SHOP</button>
-      </Link>
-    </header>
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      <Box p={2}>{children}</Box>
+    </Typography>
   )
 }
 
-export default Header
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+}
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  }
+}
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}))
+
+export default function SimpleTabs() {
+  const classes = useStyles()
+  const [value, setValue] = React.useState(0)
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="simple tabs example"
+        >
+          <Link to="/go-shop/">
+            <Tab label="Home" {...a11yProps(0)} />
+          </Link>
+          <Link to="/go-shop/lists">
+            <Tab label="Lists" {...a11yProps(1)} />
+          </Link>
+          <Link to="/go-shop/myList">
+            <Tab label="My Lists" {...a11yProps(2)} />
+          </Link>
+          <Link to="/go-shop/shoppingList">
+            <Tab label="Let's Shop" {...a11yProps(3)} />
+          </Link>
+        </Tabs>
+      </AppBar>
+    </div>
+  )
+}
