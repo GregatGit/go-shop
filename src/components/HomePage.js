defca215649a52e628f9f9c62a0d579425aeddc4
gregatgit <gregdd@outlook.com>
2019-10-01 16:15:20 +1000

    add paper background to all

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,9 @@
 import React, { Fragment, Component } from 'react'
 import { connect } from 'react-redux'
 import { BrowserRouter, Route } from 'react-router-dom'
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
 import Lists from './Lists'
 import Header from './Header'
 import Welcome from './Welcome'
@@ -9,22 +12,29 @@ import List from './List'
 import TheList from './TheList'
 import ShoppingList from './ShoppingList'
 
-class HomePage extends Component {
-  render() {
-    return (
-      <Fragment>
-        <BrowserRouter>
-          <Header />
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2),
+  },
+}))
+
+function HomePage() {
+  const classes = useStyles()
+  return (
+    <Fragment>
+      <BrowserRouter>
+        <Header />
+        <Paper className={classes.root}>
           <Route path="/" exact component={Welcome} />
           <Route path="/go-shop/" exact component={Welcome} />
           <Route path="/go-shop/lists" exact component={Lists} />
           <Route path="/go-shop/lists/:id" component={TheList} />
           <Route path="/go-shop/myList" component={MyList} />
           <Route path="/go-shop/shoppingList" component={ShoppingList} />
-        </BrowserRouter>
-      </Fragment>
-    )
-  }
+        </Paper>
+      </BrowserRouter>
+    </Fragment>
+  )
 }
 
 const mapStateToProps = (state, ownProps) => {
