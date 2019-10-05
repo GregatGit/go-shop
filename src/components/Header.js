a361514d15cf1a8222b6fc6ddbd626906323200c
gregatgit <gregdd@outlook.com>
2019-10-05 12:43:47 +1000

    add shopping count to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles'
 import { AppBar, Tabs, Tab } from '@material-ui/core'
 import { Link } from 'react-router-dom'
@@ -17,10 +18,18 @@ const useStyles = makeStyles(theme => ({
   },
 }))
 
-export default function SimpleTabs() {
+function Header({list}) {
   const classes = useStyles()
   const [value, setValue] = React.useState(0)
 
+  let amount = 0
+  list.forEach((item) => {
+    if(!item.done){
+      amount++
+    }
+  })
+
+  console.log(amount)
   const handleChange = (event, newValue) => {
     setValue(newValue)
   }
@@ -36,9 +45,17 @@ export default function SimpleTabs() {
           <Tab label="Home" {...a11yProps(0)} component={Link} to="/go-shop/"/>
           <Tab label="Lists" {...a11yProps(1)} component={Link} to="/go-shop/lists" />
           <Tab label="My List" {...a11yProps(2)} component={Link} to="/go-shop/myList"/>
-          <Tab label="Let's Shop" {...a11yProps(3)} component={Link} to="/go-shop/shoppingList"/>
+          <Tab label={"Let's Shop " + amount} {...a11yProps(3)} component={Link} to="/go-shop/shoppingList"/>
         </Tabs>
       </AppBar>
     </div>
   )
 }
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    list: state.shoppingList
+  }
+}
+
+export default connect(mapStateToProps)(Header)
