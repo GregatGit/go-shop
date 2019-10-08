bcf1275aca1db7b582cb94ee758a1867a65f3a97
gregatgit <gregdd@outlook.com>
2019-10-08 12:04:41 +1000

    add about dialog

---
+++ src/components/Dialog/About.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import {Button, Fab} from '@material-ui/core'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import Slide from '@material-ui/core/Slide'
+import EditIcon from '@material-ui/icons/Edit'
+import InfoIcon from '@material-ui/icons/Info'
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />
+})
+
+const useStyles = makeStyles(theme => ({
+  fab: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
+function About() {
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(false)
+
+  const handleClickOpen = () => {
+    setOpen(true)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
+  }
+
+  return (
+    <div>
+      <Fab onClick={handleClickOpen} color="secondary" aria-label="edit" className={classes.fab}>
+        about
+      </Fab>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title">
+          {"Go-Shop: created by Greg Duncan"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+          I built Go-Shop to practice my react and material-ui skills.
+          And it makes shopping easier.
+          You can see some of my other projects at my GitHub profile.
+          If you have any issues please email me at mygoshop101@gmail.com.
+          Please enjoy.         
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+        <a href="mailto:mygoshop101?subject=contact_from_website&body=Hello"><Button onClick={handleClose} color="primary">
+            Email
+          </Button></a>
+          <Button onClick={() =>
+            window.open(
+              'https://github.com/GregatGit?tab=repositories',
+              '_blank'
+            )} color="primary">
+            github profile
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  )
+}
+
+export default About 
+
