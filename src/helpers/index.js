0a792151991b18a2d9c21699c0982fb7d9c5b30d
gregatgit <gregdd@outlook.com>
2019-10-08 10:34:14 +1000

    make sure names are valid

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -10,3 +10,16 @@ export const getItems = (list, code) => {
     return item.lists.indexOf(code) > -1
   })
 }
+
+// checks that name is acceptable
+export function checkName(name) {
+  let str = name.trim()
+  const error = { errorStatus: false, message: ''}
+  if (str.length < 3){
+    error.errorStatus = true
+    error.message = 'name must be 3 letters or more'
+    return error
+  }
+
+  return error
+}
