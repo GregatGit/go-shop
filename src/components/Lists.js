d5d1e71379fc1f71b0ff701ad5ab367f95fc2b5c
gregatgit <gregdd@outlook.com>
2019-10-16 17:09:39 +1000

    add search bar to list

--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState} from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { displayName } from '../helpers'
 import { makeStyles } from '@material-ui/core/styles'
 import { Button } from '@material-ui/core'
 import Create from './Dialog/Create'
+import SearchBar from 'material-ui-search-bar'
 
 const useStyles = makeStyles(theme => ({
   button: {
@@ -17,9 +18,10 @@ const useStyles = makeStyles(theme => ({
 
 const Lists = ({ lists, categories }) => {
   const classes = useStyles()
+  const [search, setSearch] = useState('')
   const listOfList = Object.keys(lists)
 
-  const createCategoryButtons = (cats) => {
+  const createCategoryButtons = cats => {
     return cats.map(name => {
       const display = displayName(name)
       return (
@@ -28,7 +30,7 @@ const Lists = ({ lists, categories }) => {
             <Button
               variant="contained"
               color="primary"
-              className={classes.button}           
+              className={classes.button}
             >
               {display}
             </Button>
@@ -38,19 +40,20 @@ const Lists = ({ lists, categories }) => {
     })
   }
 
-  const addOwnItem = () => {
-    console.log('add!')
-  }
-
   return (
     <div>
       <h1>LISTS</h1>
       <h3>press to see items in list</h3>
-      <p>items won't show if they are already on your list</p>   
+      <p>items won't show if they are already on your list</p>
       <div>{createCategoryButtons(listOfList)}</div>
       <div>{createCategoryButtons(categories)}</div>
-      <Button onClick={addOwnItem}>ADD MY OWN ITEM</Button>
+      <Button >ADD MY OWN ITEM</Button>
       <Create />
+      <SearchBar
+        value={search}
+        onChange={newValue => setSearch(newValue)}
+        onRequestSearch={() => console.log(search)}
+      />
     </div>
   )
 }
@@ -59,7 +62,7 @@ const mapStateToProps = (state, ownProps) => {
   return {
     lists: state.lists,
     amount: state.shoppingList.length,
-    categories: state.categories
+    categories: state.categories,
   }
 }
 
